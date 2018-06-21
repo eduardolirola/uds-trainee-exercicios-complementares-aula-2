e8c9da0e000f8a8a950d77e43a5e87079ca699de
eduardolirola <eduardolirola@bol.com.br>
2018-06-21 12:41:53 -0300

    exercicio5

---
+++ Exercicio-4/src/Controlador.java
@@ -0,0 +1,15 @@
+public interface Controlador {
+
+    void ligar();
+    void desligar1();
+    void abrirMenu();
+    void fecharMenu();
+    void maisVolume();
+    void menosVolume();
+    void ligarMudo();
+    void desligarMudo();
+    void play();
+    void pouse();
+
+
+}
---
+++ Exercicio-4/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        ControleRemoto controle = new ControleRemoto();
+
+        controle.ligar();
+        controle.maisVolume();
+        controle.menosVolume();
+        controle.menosVolume();
+        controle.menosVolume();
+        controle.maisVolume();
+        controle.play();
+        controle.maisVolume();
+        controle.ligarMudo();
+        controle.menosVolume();
+        controle.abrirMenu();
+
+
+
+    }
+
+}
